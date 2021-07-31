7e103a83471b664035ba577637731b711fb5240c
mgsanj <36062147+mgsanj@users.noreply.github.com>
2021-07-30 20:24:01 -0700

    Create lab1prep.cpp

---
+++ lab1prep.cpp
@@ -0,0 +1,16 @@
+//  Sanjana Gadaginmath, July 2
+//  Created by Sanjana Gadaginmath on 07/02/2021.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   cout << "/*****************************************************/" << endl;
+   cout << "/*                                                   */" << endl;
+   cout << "/*                  Hello, C++!                      */" << endl;
+   cout << "/*                                                   */" << endl;
+   cout << "/*****************************************************/" << endl;
+
+   return 0;
+}
