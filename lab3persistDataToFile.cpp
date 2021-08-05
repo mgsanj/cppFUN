b34c4d08ec9bc0f707752c065661927a1beb03f3
mgsanj <36062147+mgsanj@users.noreply.github.com>
2021-08-05 14:06:07 -0700

    Update lab3persistDataToFile.cpp

--- lab3persistDataToFile.cpp
+++ lab3persistDataToFile.cpp
@@ -1,3 +1,38 @@
+/*
+Steps
+
+    1. Define and Initialize Variables
+        Define seven string variables and initialize them with the days of a week. Define one variable per line. Use meaningful names for the variables. For example, string day1 = "Sunday"; for the first day of the week.
+
+        Define seven double variables and initialize them with the expenses of the week. Define one variable per line. Use meaningful names for the variables. For example, double day1Exp = 45.25; for the expense of the first day. Use 49.38, 12.75, 123.5, 8.25, 19.95, 145.7 for the rest of the days.
+
+    2. Create a Data File
+        Define a named string constant for the file name, “expenses.txt”. Use a meaningful name for the variable. The name must be in uppercase.
+
+        Define an ofstream variable. Use a meaningful name for the variable. For example, outFile.
+
+        Use the ofstream variable to open the file using the named string constant defined above.
+
+        Use the insertion operator << to insert the variable for the first day, followed by a space and then the variable for the expense of the first day in that order.
+
+        Do the same thing for the other six days and expenses.
+
+    3. Close The File
+        Use the ofstream variable to close the file.
+
+    4. Report
+        Inform the user that the file expenses.txt has been created.
+
+        Use cout to inform the user that the file expenses.txt has been created. You must use the named constant for the file name in the cout statement. That is, the following statement will not be accepted.
+
+        cout << "File expenses.txt has been created." << endl;
+
+        Run and Check The File
+
+        Test your code and see if the output file, expenses.txt, and the program output are exactly the same as the expected.
+
+*/
+
 //
 //  main.cpp
 //  labs
