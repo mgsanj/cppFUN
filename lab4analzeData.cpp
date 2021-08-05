540c5c47e2b159e25c79c361464f3ee7859e0138
mgsanj <36062147+mgsanj@users.noreply.github.com>
2021-08-05 14:08:11 -0700

    Update lab4analzeData.cpp

--- lab4analzeData.cpp
+++ lab4analzeData.cpp
@@ -1,3 +1,65 @@
+/*
+Follow the steps described here to complete the program.
+
+Collecting Data From The keyboard input (cin).
+
+Define a named constant for the size of the arrays. For example, const int NUM_DAYS {7};
+
+Define an array of strings for the days and an array of doubles for the expenses using the named constant 
+as the array size. Initialize the array of doubles with 0.0, not 0. Each day in the array of days corresponds 
+to the expense in the same position in the array of expenses.
+
+Check. The arrays have been defined using meaningful identifiers.
+
+Check. The arrays have been defined using the named constant as the array size.
+
+Check. The array of doubles have been initialized with 0.0.
+
+Read the data from the keyboard into the arrays. The code for reading the expense records should not assume there 
+are exactly seven records in the file. Use a counter variable to keep track of the number of records received from
+the keyboard. Other than reading and counting the number of expense records, don't do any data processing and reporting 
+at this step.
+
+Check. The number 7 is not used in any form in the process of getting data from cin.
+
+Check. No calculation for total, average, and largest at this step.
+
+Check. No reporting at this step.
+
+Process Data
+
+Process the data in the arrays. Calculate the total, average, and largest expense.
+
+Create Report
+
+For keyboard input.
+
+ Sunday 45.25
+ Monday 49.38
+ Tuesday 12.75
+ Wednesday 123.5
+ Thursday 8.25
+ Friday 19.95
+ Saturday 145.7
+ End
+ 
+Generate the expenses report as shown below.
+
+               My Weekly Expense Report
+               ------------------------
+               Sunday              45.25
+               Monday              49.38
+               Tuesday             12.75
+               Wednesday          123.50
+               Thursday             8.25
+               Friday              19.95
+               Saturday           145.70
+Total of expenses:                404.78
+Average of expenses:               57.83
+Largest of expenses:              145.70
+
+*/
+
 //
 // expensearr.cpp
 // labs22A
